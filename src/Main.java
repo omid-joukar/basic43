6bddc400cdea599c6ea6a72a23a884f839cf65c3
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-09 15:25:30 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by KPS on 7/9/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Twinkle, twinkle, little star,\n" +
+                "\tHow I wonder what you are! \n" +
+                "\t\tUp above the world so high,   \t\t\n" +
+                "\t\tLike a diamond in the sky. \n" +
+                "Twinkle, twinkle, little star, \n" +
+                "\tHow I wonder what you are");
+    }
+}
